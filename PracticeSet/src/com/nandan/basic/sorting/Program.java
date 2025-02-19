db9ec9b2bf3be6c3e746a9bd91881e5ccc6f5bcc
Nandan2890 <nkb2890@gmail.com>
2025-02-19 12:41:29 +0530

    sorting algorithms in java

--- PracticeSet/src/com/nandan/basic/sorting/Program.java
+++ PracticeSet/src/com/nandan/basic/sorting/Program.java
@@ -6,7 +6,10 @@ public class Program {
 	public static void main(String[] args) {
 		int[] arr = {13, 46, 24, 52, 20, 9};
 		
-		selectionSort(arr, arr.length);
+//		selectionSort(arr, arr.length);
+//		bubbleSort(arr, arr.length);
+		insertionSort(arr, arr.length);
+		
 		
 		System.out.println(Arrays.toString(arr));
 	}
@@ -28,6 +31,36 @@ public class Program {
 		int temp = arr[mini];
 		arr[mini] = arr[i];
 		arr[i] = temp;
+	}
+	
+	public static void bubbleSort(int[] arr, int n) {
+//		for(int i = 0; i < n-1; i++) {
+//			for(int j = 0; j < n-i-1; j++) {
+//				if(arr[j] > arr[j+1]) {
+//					swap(arr, j, j+1);
+//				}
+//			}
+//		}
+		
+//		method 2 start from last index
+		for(int i=n-1; i > 0; i--) {
+			for(int j=0; j<=i-1; j++) {
+				if(arr[j] > arr[j+1]) {
+					swap(arr, j, j+1);
+				}
+			}
+		}
+	}
+	
+	
+	public static void insertionSort(int[] arr, int n) {
+		for(int i = 1; i < n; i++) {
+			int j = i;
+			while(j > 0 && arr[j-1] > arr[j]) {
+				swap(arr, j-1, j);
+				j--;
+			}
+		}
 	}
 	
 }
