73007d429a6cd1091d2a0416cfbc2c7a146ff976
Nandan2890 <nkb2890@gmail.com>
2025-02-19 23:59:50 +0530

    removeLast() method added

--- PracticeSet/src/com/nandan/basic/linkedlist/Main.java
+++ PracticeSet/src/com/nandan/basic/linkedlist/Main.java
@@ -1,8 +1,18 @@
 package com.nandan.basic.linkedlist;
 
+import com.nandan.basic.linkedlist.LinkedList.Node;
+
 public class Main {
 	public static void main(String[] args) {
-		LinkedList myLinkedList = new LinkedList(4);
+		LinkedList myLinkedList = new LinkedList(1);
+		
+		myLinkedList.append(2);
+		myLinkedList.append(3);
+		myLinkedList.append(4);
+		myLinkedList.append(5);
+		
+		Node node = myLinkedList.removeLast();
+		System.out.println(node.value);
 		
 		myLinkedList.getHead();
 		myLinkedList.getTail();
