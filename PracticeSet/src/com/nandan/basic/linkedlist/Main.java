ee1a133e8a7c8f35bf308edf0b04d948cc431fc3
Nandan2890 <nkb2890@gmail.com>
2025-02-20 00:19:10 +0530

    feature1

--- PracticeSet/src/com/nandan/basic/linkedlist/Main.java
+++ PracticeSet/src/com/nandan/basic/linkedlist/Main.java
@@ -6,16 +6,18 @@ public class Main {
 	public static void main(String[] args) {
 		LinkedList myLinkedList = new LinkedList(1);
 		
-		myLinkedList.append(2);
-		myLinkedList.append(3);
-		myLinkedList.append(4);
-		myLinkedList.append(5);
+//		myLinkedList.append(2);
+//		myLinkedList.append(3);
+//		myLinkedList.append(4);
+//		myLinkedList.append(5);
 		
 		Node node = myLinkedList.removeLast();
 		System.out.println(node.value);
 		
-		myLinkedList.getHead();
-		myLinkedList.getTail();
+		System.out.println(myLinkedList.removeLast());
+		
+//		myLinkedList.getHead();
+//		myLinkedList.getTail();
 		myLinkedList.getLength();
 		
 		myLinkedList.printList();
