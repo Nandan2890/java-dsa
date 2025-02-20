b6c54f9693bf590c9117eaa00552fba9494ef173
Nandan2890 <nkb2890@gmail.com>
2025-02-20 16:41:10 +0530

    completion of linked list

--- PracticeSet/src/com/nandan/basic/linkedlist/LinkedList.java
+++ PracticeSet/src/com/nandan/basic/linkedlist/LinkedList.java
@@ -68,6 +68,121 @@ public class LinkedList {
 		return temp;
 	}
 	
+//	removeFirst() method to remove the first item from the linkedList
+	public Node removeFirst() {
+		if(length==0) {
+			return null;
+		}
+		Node temp = head;
+		head = head.next;
+		temp.next = null;
+		length--;
+		
+		if(length == 0) {
+			tail=null;
+		}
+		
+		return temp;
+	}
+	
+//	prepend()method to add item at the first of the linkedList
+	public void prepend(int value) {
+		Node newNode = new Node(value);
+		if(length==0) {
+			head = newNode;
+			tail = newNode;
+		}else {
+			newNode.next = head;
+			head = newNode;
+		}
+		
+		length++;
+	}
+	
+//	get(index) get the item from the specific index in the linkedList
+	public Node get(int index) {
+		if(index < 0 || index >= length) return null;
+		
+		Node temp = head;
+		
+		for(int i = 0; i < index; i++) {
+			temp = temp.next;
+		}
+		
+		return temp;
+	}
+	
+//	set(index, value) change the value at the specific index
+	public boolean set(int index, int value) {
+		Node temp = get(index);
+		if(temp != null) {
+			temp.value = value;
+			return true;
+		}
+		
+		return false;
+	}
+	
+//	//insert(int index, int value) insert the item at the specific index and push other item next and length will increase
+    public boolean insert(int index, int value){
+        if(index < 0 || index > length) return false;
+
+        if(index==0){
+            prepend(value);
+            return true;
+        }
+
+        if(index==length){
+            append(value);
+            return true;
+        }
+
+        Node newNode = new Node(value);
+        Node temp = get(index-1);
+
+        newNode.next = temp.next;
+        temp.next = newNode;
+        length++;
+
+        return true;
+    }
+    
+    public Node remove(int index){
+        if(index < 0 || index >= length){
+            return null;
+        }
+        if(index == 0){
+            return removeFirst();
+        }
+        if(index==length-1){
+            return removeLast();
+        }
+
+        Node prev = get(index-1);
+        Node temp = prev.next;
+
+        prev.next = temp.next;
+        temp.next = null;
+        length--;
+
+        return temp;
+    }
+    
+ // public void reverse() is a method to reverse the linkedlist
+    public void reverse(){
+        Node temp = head;
+        head = tail;
+        tail = temp;
+
+        Node after = temp.next;
+        Node before = null;
+        for(int i = 0; i < length; i++){
+            after = temp.next;
+            temp.next = before;
+            before = temp;
+            temp = after;
+        }
+    }
 	
 //	method to get head
 	public void getHead() {
