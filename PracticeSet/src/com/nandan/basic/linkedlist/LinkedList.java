fc3c5a906b36ae4492412814df46f6930ebcaf4c
Nandan2890 <nkb2890@gmail.com>
2025-02-22 16:22:41 +0530

    partition-list

--- PracticeSet/src/com/nandan/basic/linkedlist/LinkedList.java
+++ PracticeSet/src/com/nandan/basic/linkedlist/LinkedList.java
@@ -278,4 +278,36 @@ public class LinkedList {
         return null;
 	}
 	
+	
+	public void partitionList(int x) {
+		if(head == null) {
+			return;
+		}
+		
+		Node dummy1 = new Node(0);
+		Node dummy2 = new Node(0);
+		Node prev1 = dummy1;
+		Node prev2 = dummy2;
+		
+		
+		Node current = head;
+		
+		while(current != null) {
+			if(current.value < x) {
+				prev1.next = current;
+				prev1 = current;
+			}else {
+				prev2.next = current;
+				prev2 = current;
+			}
+			
+			current = current.next;
+		}
+		
+		prev2.next = null;
+		head = dummy1.next;
+		prev1.next = dummy2.next;
+		
+	}
+	
 }
